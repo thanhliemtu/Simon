2302a2d493bbbef8ae2252138bd32c5887494ab3
Thanh Liem Tu <thanhliem.tu@mail.utoronto.ca>
2023-02-26 20:51:40 -0500

    Added a way to initialize the array

--- starter.s
+++ starter.s
@@ -1,6 +1,20 @@
 .data
-sequence:  .byte 0,0,0,0
-count:     .word 4
+count:            .word 4
+
+red:              .byte 0,0,255
+.align             4
+green:            .byte 0,255,0
+.align             4
+blue:             .byte 255,0,0
+.align             4
+yellow:           .byte 0,255,255
+.align             4
+black:            .byte 0,0,0
+.align             4
+white:            .byte 255,255,255
+.align             4
+
+sequence:         .byte 0,0,0,0
 
 .globl main
 .text
@@ -12,7 +26,22 @@ main:
     # LEFT, and 3 for RIGHT. Store the sequence in memory. We provided 
     # a declaration above that you can use if you want.
     # HINT: Use the rand function provided to generate each number
-
+    
+# This loop initializes the array with random numbers
+array_init_loop_init:
+    li s2, 0 # s2 = 0
+    lw s3, count # s3 = count
+    la s4, sequence # s4 = sequence[]
+array_init_loop_body:
+    bge s2, s3, array_init_loop_end # check if s2 >= s3, if yes, end
+    slli s5, s2, 2 # s5 = s2 * 4
+    add s5, s5, s4 # s5 is address of sequence[s2]
+    li a0, 4 # Putting 4 into a0
+    call rand # Calling rand with a0 = 10, so rand(10)
+    sb a0, 0(s5) # sequence[s2] = a0 (which is a random num)
+    addi s2, s2, 1 # s2 = s2 + 1
+    j array_init_loop_body
+array_init_loop_end:
    
     # TODO: Now read the sequence and replay it on the LEDs. You will
     # need to use the delay function to ensure that the LEDs light up 
@@ -22,7 +51,7 @@ main:
     # 2. Wait for a short delay (e.g. 500 ms)
     # 3. Turn off the LED (i.e. set it to black)
     # 4. Wait for a short delay (e.g. 1000 ms) before repeating
-    
+
     
     # TODO: Read through the sequence again and check for user input
     # using pollDpad. For each number in the sequence, check the d-pad
@@ -42,7 +71,8 @@ exit:
     
 # --- HELPER FUNCTIONS ---
 # Feel free to use (or modify) them however you see fit
-     
+
+# DELAY FUNCTION
 # Takes in the number of milliseconds to wait (in a0) before returning
 delay:
     mv t0, a0
@@ -57,7 +87,9 @@ delayLoop:
 delayIfEnd:
     bltu t2, t0, delayLoop
     jr ra
+# END OF DELAY FUNCTION
 
+# RANDOM GENERATOR FUNCTION
 # Takes in a number in a0, and returns a (sort of) random number from 0 to
 # this number (exclusive)
 rand:
@@ -66,7 +98,8 @@ rand:
     ecall
     remu a0, a0, t0
     jr ra
-    
+# END OF RANDOM GENERATOR FUNCTION
+
 # Takes in an RGB color in a0, an x-coordinate in a1, and a y-coordinate
 # in a2. Then it sets the led at (x, y) to the given color.
 setLED:
